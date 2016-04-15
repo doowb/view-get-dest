a2ad7a9a6998751faf8fbb46ac5d1d5d548f867e
Brian Woodward <brian.woodward@gmail.com>
2016-04-15 11:10:19 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * view-get-dest <https://github.com/doowb/view-get-dest>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
